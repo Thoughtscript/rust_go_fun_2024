9163dfb3f3319d005fd05905a33dfd5a4da3d183
Adam Gerard <adam.intae.gerard@protonmail.com>
2024-05-15 20:29:55 -0500

    slightly better log formatting

--- curl_test_all.sh
+++ curl_test_all.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 bash curl_test_go_mongo.sh
-bash curl_test_rust_actix.sh
-bash curl_test_rust_mongo.sh
-bash curl_test_workers.sh
+sleep 5 & bash curl_test_rust_actix.sh
+sleep 10 & bash curl_test_rust_mongo.sh
+sleep 15 & bash curl_test_workers.sh
